6fd6d66f950ad6bba3510247b2475aa3731a2f59
tahoito <taho.ito0304@gmail.com>
2025-02-10 11:26:53 +0900

    更新

--- diaryproject/diary/views.py
+++ diaryproject/diary/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from .forms import DiaryForm
+from django.urls import reverse_lazy
+
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class DiaryCreateView(CreateView):
+    template_name = 'diary_create.html'
+    form_class = DiaryForm
+    success_url = reverse_lazy('diary:diary_create_complete')
