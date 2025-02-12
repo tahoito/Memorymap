60f6449ee53c4c9a6d0238c7b6920e6d8056ab43
tahoito <taho.ito0304@gmail.com>
2025-02-12 17:21:21 +0900

    update.html作成

--- diaryproject/diary/views.py
+++ diaryproject/diary/views.py
@@ -1,13 +1,12 @@
+from time import timezone
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView
-from django.views.generic import CreateView,DetailView
+from django.views.generic import CreateView,DetailView,UpdateView
 
 from diary.models import Diary
 
 from .forms import DiaryForm
 from django.urls import reverse_lazy
-
-
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -27,3 +26,16 @@ class DiaryListView(ListView):
 class DiaryDetailView(DetailView):
     template_name = 'diary_detail.html'
     model = Diary
+
+
+
+class DiaryUpdateView(UpdateView):
+    template_name = 'diary_update.html'
+    model = Diary
+    fields = ('date', 'title', 'text',)
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.save()
+        return super().form_valid(form)
