3a6abb6ae61af48e2c845683e567c7edca92cf6d
tahoito <taho.ito0304@gmail.com>
2025-02-19 21:46:15 +0900

    更新

--- diaryproject/diary/models.py
+++ diaryproject/diary/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 import uuid
 
 class Diary(models.Model):
@@ -9,5 +10,10 @@ class Diary(models.Model):
     text = models.CharField(verbose_name='本文',max_length=200)
     created_at = models.DateTimeField(verbose_name='作成日時',default=timezone.now())
     updated_at = models.DateTimeField(verbose_name='編集日時',blank=True,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
+class Todo(models.Model):
+    time = models.DateField(verbose_name="作成日", default=timezone.now)
+    memo = models.TextField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
--- diaryproject/diary/urls.py
+++ diaryproject/diary/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'diary'
 urlpatterns = [
-    path('index/', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('diary/create/',views.DiaryCreateView.as_view(),name='diary_create'),
     path('diary/create/complete/',views.DiaryCreateCompleteView.as_view(),name='diary_create_complete'),
     path('diary/list/',views.DiaryListView.as_view(),name='diary_list'),
