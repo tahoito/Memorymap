e4eed19e2feb18acdc35e60d98665a8771ba070b
tahoito <taho.ito0304@gmail.com>
2025-02-22 00:08:27 +0900

    更新

--- diaryproject/config/settings.py
+++ diaryproject/config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diary',
+    'bootstrap4',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins': [
+                'bootstrap4.templatetags.bootstrap4', 
             ],
         },
     },
